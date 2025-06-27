81a45e8fa76d5bb6de4ffb40cf49849cc10538a7
Abdur55 <mrrahim972@gmail.com>
2025-06-27 07:10:43 +0600

    added all the thing

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('donat-btn').addEventListener('click', function(event){
+    event.preventDefault
+    console.log('this is donation button')
+})
---
+++ js/shared.js
@@ -0,0 +1,5 @@
+function gettextidbyshardfunction(id){
+   const sheard1 =  document.getElementById(id).value
+   return sheard1
+
+}
